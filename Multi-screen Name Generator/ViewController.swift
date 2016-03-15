8ead14489caf11261eb7f09a6719cbb7361412e5
Yukai Ma <yukaima@dyn-118-139-11-155.its.monash.edu.au>
2016-03-15 14:03:56 +1100

    Initial Commit

---
+++ Multi-screen Name Generator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multi-screen Name Generator
+//
+//  Created by Yukai Ma on 15/03/2016.
+//  Copyright © 2016 Yukai Ma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
