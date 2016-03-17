7e6942c7574ae327610533f500215840013a2bbf
mayukai0612 <mayukai0612@gmail.com>
2016-03-17 16:59:48 +1100

    kk
    lll

---
+++ Multi-screen Name Generator/StarWarsName.swift
@@ -0,0 +1,36 @@
+//
+//  StarWarsName.swift
+//  Multi-screen Name Generator
+//
+//  Created by Yukai Ma on 15/03/2016.
+//  Copyright © 2016 Yukai Ma. All rights reserved.
+//
+
+import UIKit
+
+class StarWarsName: NSObject {
+
+    var FirstName: String?
+    var SurName: String?
+    var MothersMaidenName: String?
+    var BirthPlace :String?
+    var FavouriteBrand: String?
+    
+    override init() {
+        self.FirstName = "unknown"
+        self.SurName = "unknown"
+        self.MothersMaidenName = "unknown"
+        self.BirthPlace = "unknown"
+        self.FavouriteBrand = "unknown"
+    }
+    init(FirstName: String,SurName: String,MothersMaidenName: String,BirthPlace:String,FavouriteBrand: String) {
+        self.FirstName = FirstName
+        self.SurName = SurName
+        self.MothersMaidenName = MothersMaidenName
+        self.BirthPlace = BirthPlace
+        self.FavouriteBrand = FavouriteBrand
+    }
+    
+    
+    
+}
---
+++ Multi-screen Name Generator/ViewStarWarController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewStarWarController.swift
+//  Multi-screen Name Generator
+//
+//  Created by Yukai Ma on 15/03/2016.
+//  Copyright © 2016 Yukai Ma. All rights reserved.
+//
+
+import UIKit
+
+class ViewStarWarController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
